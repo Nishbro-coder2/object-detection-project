80775a50e6588ca722b738b2be71747c7f4fe946
Nishbro-coder2 <98819146+Nishbro-coder2@users.noreply.github.com>
2022-04-08 12:01:28 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,29 @@
+img = "";
+status = ""; 
+
+function preload(){
+    img = loadImage("book.jpeg"); 
+}
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',ModelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function draw(){
+    image(img ,0,0 ,640,420 );
+}
+function ModelLoaded(){
+    console.log("Model loaded!");
+    status = true;
+    objectDetector.detect(img , gotResults);
+}
+function gotResults(error , results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+
+    }
+}
